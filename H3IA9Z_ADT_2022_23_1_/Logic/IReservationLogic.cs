cac5ca51cb4a95713a9bf61cc1cf47fded2be99d
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 23:24:02 +0100

    Updated IReservationLogic and IVisitor logic - added props and functs

--- H3IA9Z_ADT_2022_23_1_/Logic/IReservationLogic.cs
+++ H3IA9Z_ADT_2022_23_1_/Logic/IReservationLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace H3IA9Z_ADT_2022_23_1_Logic
 {
-    internal interface IReservationLogic
+    public interface IReservationLogic
     {
+        void UpdateReservationDate(Reservation rsv);
+        public Reservation AddNewReservation(Reservation rsv);
+        public void DeleteReservation(int id);
+        Reservation GetReservation(int id);
+        IEnumerable<Reservation> GetAllReservations();
     }
 }
--- H3IA9Z_ADT_2022_23_1_/Logic/IVisitorLogic.cs
+++ H3IA9Z_ADT_2022_23_1_/Logic/IVisitorLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace H3IA9Z_ADT_2022_23_1_Logic
 {
     public interface IVisitorLogic
     {
-    }
+        void UpdateAddress(Visitor vis);
+
+        public Visitor AddNewVis(Visitor vis);
+        public void DeleteVisitor(int id);
+        Visitor GetVisitor(int id);
+        IEnumerable<Visitor> GetAllVisitors();
+
+        List<KeyValuePair<int, int>> BestVisitor();
+        List<KeyValuePair<int, int>> WorstVisitor();
+        int ReservationsNumber(int id);
+}
 }
