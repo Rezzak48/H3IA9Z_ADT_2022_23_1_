f2c35d58aa3dc5f3ce5dbe3c846f48c2a1fe6f2a
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 02:05:47 +0100

    Created Movie class

---
+++ H3IA9Z_ADT_2022_23_1_/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            this.Reservation = new HashSet<Reservation>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+        public int Duration { get; set; }
+
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"\n{this.Id,3} |  {this.Duration} hours {this.Price,10} MAD {this.Category,10}\t {this.Name,-1}";
+        }
+
+        public virtual ICollection<Reservation> Reservation { get; }
+    }
+}
