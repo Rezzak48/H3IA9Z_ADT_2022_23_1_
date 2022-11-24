f9759875bf1a864477594167abc12a7e97aa99e5
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 21:40:54 +0100

    Added IRepository interface

---
+++ H3IA9Z_ADT_2022_23_1_/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3IA9Z_ADT_2022_23_1_Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        void Insert(T entity); // Add
+        void Remove(T entity); // Delete
+    }
+}
