93d3ce536ac6092482013ef3c8dc086a3b969b45
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 23:19:58 +0100

    Updated ImovieLogic - added props and funcs

--- H3IA9Z_ADT_2022_23_1_/Logic/IMovieLogic.cs
+++ H3IA9Z_ADT_2022_23_1_/Logic/IMovieLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace H3IA9Z_ADT_2022_23_1_Logic
 {
-    internal interface IMovieLogic
+    public interface IMovieLogic
     {
+        public Movie AddNewArtist(Movie newMovie);
+        public void DeleteMovie(int id);
+        Movie GetMovie(int id);
+        IEnumerable<Movie> GetAllMovies();
+        void UpdateMovieCost(Movie value);
+
+        IEnumerable<KeyValuePair<string, int>> MovieEarnings();
+        List<KeyValuePair<string, int>> MostSellMovie();
+        List<KeyValuePair<string, int>> LessSellMovie();
     }
 }
