b2110cbea358830c8f56344ee5a74910f6ab15be
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 23:28:11 +0100

    Created MovieLogic, ReservationLogic, VisitorLogic classes

---
+++ H3IA9Z_ADT_2022_23_1_/Logic/ReservationLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3IA9Z_ADT_2022_23_1_Logic
+{
+    internal class ReservationLogic
+    {
+    }
+}
