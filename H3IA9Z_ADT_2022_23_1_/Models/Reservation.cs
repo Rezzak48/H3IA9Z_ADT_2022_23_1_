b76f6e0804f142aa8a87db2ac54bfb60dee9bc3a
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 01:56:18 +0100

    Created Reservation class in Models

---
+++ H3IA9Z_ADT_2022_23_1_/Models/Reservation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Reservation
+    {
+        public Reservation()
+        {
+        }
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public virtual Visitor Visitor { get; set; }
+
+
+        public int? VisitorId { get; set; }
+
+
+
+        public virtual Movie Movie { get; set; }
+
+
+        public int? MovieId { get; set; }
+
+
+
+
+        public override string ToString()
+        {
+            return $"\n{this.Id,3} | {this.VisitorId,-20} {this.DateTime.Year,10}.{this.DateTime.Month}.{this.DateTime.Day} \t{this.MovieId,15}";
+        }
+    }
+}
