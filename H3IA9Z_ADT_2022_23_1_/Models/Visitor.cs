68b8073f2fb2a20ee32991c0323ce9040a83bef1
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-24 02:01:38 +0100

    Visitor class has been added

---
+++ H3IA9Z_ADT_2022_23_1_/Models/Visitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Visitor
+    {
+        public Visitor()
+        {
+            this.Reservation = new HashSet<Reservation>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public virtual ICollection<Reservation> Reservation { get; }
+
+
+        public override string ToString()
+        {
+            return $"\n{this.Id,3} | {this.Name,-20} {this.Email,-28} {this.PhoneNumber,10}  \t {this.City}";
+        }
+    }
+
+}
