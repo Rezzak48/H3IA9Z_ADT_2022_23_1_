abc351d55c9ea8ea4c5cc926bb429fbfaeaba83e
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-29 10:40:57 +0100

    Implemented MovieEarnings, less and & most movies earning functions

--- H3IA9Z_ADT_2022_23_1_/Logic/MovieLogic.cs
+++ H3IA9Z_ADT_2022_23_1_/Logic/MovieLogic.cs
@@ -57,17 +57,37 @@ namespace H3IA9Z_ADT_2022_23_1_Logic
 
         public List<KeyValuePair<string, int>> LessSellMovie()
         {
-            throw new NotImplementedException();
+            int min = MovieEarnings().Min(t => t.Value);
+            string[] minNums = MovieEarnings().Where(x => x.Value == min).Select(x => x.Key).ToArray();
+            List<KeyValuePair<string, int>> revenu = new List<KeyValuePair<string, int>>();
+            for (int i = 0; i < minNums.Length; i++)
+            {
+                revenu.Add(new KeyValuePair<string, int>(minNums[i], min));
+            }
+            return revenu;
         }
 
         public List<KeyValuePair<string, int>> MostSellMovie()
         {
-            throw new NotImplementedException();
+            int max = MovieEarnings().Max(t => t.Value);
+            string[] maxNums = MovieEarnings().Where(x => x.Value == max).Select(x => x.Key).ToArray();
+            List<KeyValuePair<string, int>> revenu = new List<KeyValuePair<string, int>>();
+            for (int i = 0; i < maxNums.Length; i++)
+            {
+                revenu.Add(new KeyValuePair<string, int>(maxNums[i], max));
+            }
+            return revenu;
         }
 
         public IEnumerable<KeyValuePair<string, int>> MovieEarnings()
         {
-            throw new NotImplementedException();
+            var TotalEarning = from movies in this.imovieRepository.GetAll().ToList()
+                               join reservations in this.ireservationsRepository.GetAll().ToList()
+                               on movies.Id equals reservations.MovieId
+                               group reservations by reservations.MovieId.Value into gr
+                               select new KeyValuePair<string, int>
+                               (imovieRepository.GetOne(gr.Key).Name, (gr.Count()) * imovieRepository.GetOne(gr.Key).Price);
+            return TotalEarning;
         }
 
         public void UpdateMovieCost(Movie value)
