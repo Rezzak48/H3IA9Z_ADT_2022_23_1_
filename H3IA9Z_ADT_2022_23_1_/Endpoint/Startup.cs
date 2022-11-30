4c2269b3390776f580c3d91f442bedb7ba0d069e
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2022-11-30 23:48:44 +0100

    Updated endpointstartup

--- H3IA9Z_ADT_2022_23_1_/Endpoint/Startup.cs
+++ H3IA9Z_ADT_2022_23_1_/Endpoint/Startup.cs
@@ -1,6 +1,10 @@
+using Data;
+using H3IA9Z_ADT_2022_23_1_Endpoint.Services;
+using H3IA9Z_ADT_2022_23_1_Logic;
+using H3IA9Z_ADT_2022_23_1_Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,17 +16,22 @@ namespace Endpoint
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
+
+            services.AddTransient<IMovieLogic, MovieLogic>();
+            services.AddTransient<IVisitorLogic, VisitorLogic>();
+            services.AddTransient<IReservationLogic, ReservationLogic>();
+            services.AddTransient<IMovieRepository, MovieRepository>();
+            services.AddTransient< IVisitorRepository, VisitorRepository>();
+            services.AddTransient<IReservationsRepository, ReservationRepository>();
+            services.AddTransient<ChooseYourMovieDbContext, ChooseYourMovieDbContext>();
+            services.AddSignalR();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,20 +41,19 @@ namespace Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:23079"));
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapHub<SignalRHub>("/hub");
             });
         }
     }
