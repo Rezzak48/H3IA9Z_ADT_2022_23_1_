4d0797e52fe5828c766b4405eaf960fb5226efb8
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2023-04-27 08:20:20 +0200

    WpfClient project created

---
+++ H3IA9Z_ADT_2022_23_1_/H3IA9Z_ADT_2022_23_1_WpfClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace H3IA9Z_ADT_2022_23_1_WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
