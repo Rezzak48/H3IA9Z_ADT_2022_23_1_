482e5367573b8f41f469e2218456d0992eb2b7cf
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2023-04-28 09:36:16 +0200

    created windows wpf and viewmodel classes

---
+++ H3IA9Z_ADT_2022_23_1_/H3IA9Z_ADT_2022_23_1_WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3IA9Z_ADT_2022_23_1_WpfClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RelayCommand ManageMoviesCommand { get; set; }
+        public RelayCommand ManageVisitorsCommand { get; set; }
+        public RelayCommand ManageReservationsCommand { get; set; }
+        //public RelayCommand ManageServicesCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ManageMoviesCommand = new RelayCommand(OpenMoviesWindow);
+            ManageVisitorsCommand = new RelayCommand(OpenVisitorsWindow);
+            ManageReservationsCommand = new RelayCommand(OpenReservationsWindow);
+            //ManageServicesCommand = new RelayCommand(OpenServicesWindow);
+        }
+
+        //! Windows to create
+
+        private void OpenMoviesWindow()
+        {
+            new MoviesWindow().Show();
+        }
+
+        private void OpenVisitorsWindow()
+        {
+            new VisitorsWindow().Show();
+        }
+
+        private void OpenReservationsWindow()
+        {
+            new ReservationsWindow().Show();
+        }
+    }
+}
