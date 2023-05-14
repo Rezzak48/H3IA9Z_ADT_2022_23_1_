3db98060819df1ad803c25fcd921758365eba14f
MOHAMED ERRAZAK ALLAH <62334356+Rezzak48@users.noreply.github.com>
2023-05-14 13:09:57 +0200

    created JsClient with some html and js and css files.

---
+++ H3IA9Z_ADT_2022_23_1_/H3IA9Z_ADT_2022_23_1_JSClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace H3IA9Z_ADT_2022_23_1_JSClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
